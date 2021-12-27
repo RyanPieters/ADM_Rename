e29bfa20c0dd95b21ce5432b2000c6c91b4a04e3
RyanPieters <Ryan.pieters9870@gmail.com>
2021-12-27 12:38:49 +0200

    checkDuplicate function added to identify and ignore duplicate ADM codes.
    Output for users also added.

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from tika import parser
+from pathlib import Path
 import re
 import os
 
-directory = r'C:\Users\klein\Documents\My Web Sites\PyProjects\PdfRename_ADM'
+# Required Variables
+directory = Path(__file__).parent
+counter = 0
+GameIdList = []
+GameDupList = []
+
+
+def checkDuplicate(game_id):
+    duplicate = False
+
+    for x in GameIdList:
+        if x == game_id and duplicate != True:
+            duplicate = True
+            break
+
+    if duplicate:
+        GameDupList.append(game_id)
+        return None
+    else:
+        GameIdList.append(game_id)
+        return game_id
+
+
+# Main for loop
 for filename in os.listdir(directory):
     if filename.endswith(".pdf"):
         pdf_Name = filename
@@ -10,13 +34,24 @@ for filename in os.listdir(directory):
 
         raw_data_trimmed = re.sub('\s+', '', raw_data['content'])
 
-        #print(raw_data_trimmed)
-
         idHit = raw_data_trimmed.find('eidentificatoconcodiceADM:')
         idEnd = raw_data_trimmed[idHit:].find('Presentata') - 26
 
-        id = raw_data_trimmed[idHit+26:idHit+idEnd+26]
+        gameId = raw_data_trimmed[idHit+26:idHit+idEnd+26]
 
-        os.rename(filename, id + '.pdf')
+        gameId = checkDuplicate(gameId)
+
+        if gameId is not None:
+            os.rename(filename, gameId + '.pdf')
+
+print('Game IDs found and renamed:')
+for x in GameIdList:
+    print(x + '\t', end=" ")
+print('\nDuplicates found and not renamed:')
+for x in GameDupList:
+    print(x + '\t', end=" ")
+
+
+
 
 
